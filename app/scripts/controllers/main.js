1fb81358a9813b887b2bf5c8ef8bac896b4858d3
khal <khal.rahman@gmail.com>
2014-05-24 20:41:23 +0800

    added reversed method into controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -24,5 +24,9 @@ ngRehearsalApp.controller('FirstCtrl', function($scope, Data){
 
 ngRehearsalApp.controller('SecondCtrl', function($scope, Data){
     $scope.data = Data;
+
+    $scope.reversedMessage = function(message){
+        return message.split("").reverse().join("");
+    }
 });
 
