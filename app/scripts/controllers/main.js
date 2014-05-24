9ca6f970d7f25a1ea06e49bcb166f3e4476e67fd
khal <khal.rahman@gmail.com>
2014-05-24 20:36:06 +0800

    share data between controllers

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,28 @@
 'use strict';
 
-angular.module('ngRehearsalApp')
-  .controller('MainCtrl', function ($scope) {
+var ngRehearsalApp = angular.module('ngRehearsalApp');
+
+ngRehearsalApp.factory('Data', function(){
+    return {
+        message: 'I\'am data from a service.'
+    };
+});
+
+
+ngRehearsalApp.controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+        'HTML5 Boilerplate',
+        'AngularJS',
+        'Karma'
     ];
-  });
+});
+
+
+ngRehearsalApp.controller('FirstCtrl', function($scope, Data){
+    $scope.data = Data;
+});
+
+ngRehearsalApp.controller('SecondCtrl', function($scope, Data){
+    $scope.data = Data;
+});
+
