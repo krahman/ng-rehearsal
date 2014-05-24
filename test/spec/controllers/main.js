ed4ff9cf620be9fcadd4f47d7911400f377ba37d
khal <khal.rahman@gmail.com>
2014-05-24 21:25:57 +0800

    added unit test

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -19,4 +19,16 @@ describe('Controller: MainCtrl', function () {
   it('should attach a list of awesomeThings to the scope', function () {
     expect(scope.awesomeThings.length).toBe(3);
   });
+
+
+});
+
+describe('filter', function(){
+    beforeEach(module('ngRehearsalApp'));
+
+    describe('reverse', function(){
+        it('should reverse a string', inject(function(reverseFilter){
+            expect(reverseFilter('ABCD')).toEqual('DCBA');
+        }));
+    });
 });
